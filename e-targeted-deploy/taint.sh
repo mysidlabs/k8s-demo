cbf1cbf6cdf2967afa5f891f189966f628a8b15a
Steve Taranto <staranto@gmail.com>
2019-04-18 10:12:47 -0400

    Clean up taints.

--- e-targeted-deploy/taint.sh
+++ e-targeted-deploy/taint.sh
@@ -2,12 +2,10 @@
 
 if [ "$1" == "clean" ]; then
   for i in {1..4}; do
-    for l in cpu disk mem; do
-      kubectl label nodes rock${i} "taranto.dev/${l}-"
-    done
+    kubectl taint nodes rock${i} taranto.dev/month-end-
   done
 else 
   for i in {1..4}; do
-    kubectl taint  nodes rock${i} taranto.dev/month-end=true:NoSchedule
+    kubectl taint nodes rock${i} taranto.dev/month-end=true:NoSchedule
   done
 fi
