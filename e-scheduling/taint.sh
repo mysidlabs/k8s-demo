aa4bd10f3461e329dc25e3f486ae2c735082536f
Steve Taranto <staranto@gmail.com>
2020-01-20 07:15:05 -0500

    Rebranded as mysidlabs.

--- e-scheduling/taint.sh
+++ e-scheduling/taint.sh
@@ -4,9 +4,9 @@ nodes=$(kubectl get nodes --output name --selector kubernetes.io/role=node)
 
 for node in ${nodes}; do
   if [ "$1" == "clean" ]; then
-    kubectl taint node ${node} taranto.dev/month-end-
+    kubectl taint node ${node} mysidlabs.com/month-end-
   else
-    kubectl taint node ${node} taranto.dev/month-end=true:NoSchedule
+    kubectl taint node ${node} mysidlabs.com/month-end=true:NoSchedule
   fi
 done
 
