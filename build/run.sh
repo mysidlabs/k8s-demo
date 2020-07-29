d8edb06a94ad0a6f0baee9bb88847fe6bdf0fc2b
Steve Taranto <staranto@gmail.com>
2020-07-29 07:36:02 -0400

    Changes to b-storage

--- build/run.sh
+++ build/run.sh
@@ -1,18 +1,19 @@
 #! /usr/bin/env bash
 
-umask u=rw,g=rw,o=rw
+trap 'echo trapped; exit' TERM
 
 [[ ! -z "${CID}" ]] && xcid=" ${CID}" || xcid=""
 [[ ! -z "${KUBERNETES_NS}" ]] && ns="${KUBERNETES_NS}/" || ns=""
 
 [[ ! -d "/var/run/demo/${ns}" ]] && mkdir -p "/var/run/demo/${ns}"
 
-for x in {1..1000}; do
+for x in {1..100}; do
 
     printf "%s%s" "${x}" "${xcid}"
-    printf ": %s %s%s\n" "${ns}" "${HOSTNAME}" "$(date)"
+    printf ": %s %s %s\n" "${ns}" "${HOSTNAME}" "$(date)"
 
     printf "%s %s\n" "${xcid}" "$(date)" >> "/var/run/demo/${ns}${HOSTNAME}"
 
-  sleep 3
+    sleep 3 &
+    wait $!
 done
