e57d52421ed8d8028c175e7cc6980e064028f397
Steve Taranto <staranto@gmail.com>
2020-08-16 06:54:04 -0400

    Initial helm hacking

--- build/run.sh
+++ build/run.sh
@@ -2,17 +2,15 @@
 
 trap 'echo trapped; exit' TERM
 
-[[ ! -z "${CID}" ]] && xcid=" ${CID}" || xcid=""
 [[ ! -z "${KUBERNETES_NS}" ]] && ns="${KUBERNETES_NS}/" || ns=""
 
-[[ ! -d "/var/run/demo/${ns}" ]] && mkdir -p "/var/run/demo/${ns}"
+[[ ! -d "/tmp/${ns}" ]] && mkdir -p "/tmp/${ns}"
 
 for x in {1..100}; do
 
-    printf "%s%s" "${x}" "${xcid}"
-    printf ": %s %s %s\n" "${ns}" "${HOSTNAME}" "$(date)"
-
-    printf "%s %s\n" "${xcid}" "$(date)" >> "/var/run/demo/${ns}${HOSTNAME}"
+    # Don't use tee
+    printf "%s%s %s\n" "${ns}" "${HOSTNAME}" "$(date)"
+    printf "%s%s %s\n" "${ns}" "${HOSTNAME}" "$(date)" >> /tmp/${ns}${HOSTNAME}
 
     sleep 3 &
     wait $!
