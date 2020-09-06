22347bcc72329b3621930f4ca360bc5e6a566747
Steve Taranto <staranto@gmail.com>
2020-09-06 08:03:21 -0400

    Cleaned up run.sh output and dirs

--- build/run.sh
+++ build/run.sh
@@ -2,15 +2,17 @@
 
 trap 'echo trapped; exit' TERM
 
+odir="/var/run"
+
 [[ ! -z "${KUBERNETES_NS}" ]] && ns="${KUBERNETES_NS}/" || ns=""
 
-[[ ! -d "/tmp/${ns}" ]] && mkdir -p "/tmp/${ns}"
+[[ ! -d "${odir}/${ns}" ]] && mkdir -p "${odir}/${ns}"
 
 for x in {1..100}; do
 
     # Don't use tee
-    printf "%s%s %s\n" "${ns}" "${HOSTNAME}" "$(date)"
-    printf "%s%s %s\n" "${ns}" "${HOSTNAME}" "$(date)" >> /tmp/${ns}${HOSTNAME}
+    printf "%s %s %s\n" "${ns}" "${HOSTNAME}" "$(date)"
+    printf "%s %s %s\n" "${ns}" "${HOSTNAME}" "$(date)" >> ${odir}/${ns}${HOSTNAME}
 
     sleep 3 &
     wait $!
