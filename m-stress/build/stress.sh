05f3c6c90dfcc970b83f79b4845d8da90f68f41b
Steve Taranto <staranto@gmail.com>
2021-03-12 18:32:15 -0500

    Stress WIP.

--- m-stress/build/stress.sh
+++ m-stress/build/stress.sh
@@ -7,4 +7,10 @@ h=$(( $(nproc)/2 ))
 w=$(seq ${h} ${n} | sort -R | head -n 1)
 t=$(seq 4 10 | sort -R | head -n 1)
 
+echo "--- Runtime ---"
+echo n=$n
+echo h=$h
+echo w=$w
+echo t=$t
+
 stress-ng --times --wcs ${w} --timeout ${t}s 2>&1
