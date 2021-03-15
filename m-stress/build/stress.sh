bec5cc894c83a7a6dbf53c1e74b75aeed4ff997e
Steve Taranto <staranto@gmail.com>
2021-03-15 07:39:15 -0400

    Now accepts variable load and timeout.

--- m-stress/build/stress.sh
+++ m-stress/build/stress.sh
@@ -2,5 +2,15 @@
 
 cd /tmp
 
-stress-ng --metrics --cpu 0 --cpu-load 40 --timeout 10s 2>&1
+declare -a params
+saveIFS=$IFS
+IFS="/"
+params=($QUERY_STRING)
+IFS=$saveIFS
+
+timeout="${params[0]:-10}"
+load="${params[1]:-40}"
+echo "load=${load} timeout=${timeout}"
+
+stress-ng --metrics --cpu 0 --cpu-load ${load} --timeout ${timeout}s 2>&1
 
